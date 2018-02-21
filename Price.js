58cb564306aa23ccd5e20de75524e89ef2b44000
sureeyanee <yam17415@gmail.com>
2018-02-21 22:25:38 +0700

    first commit

---
+++ Price.js
@@ -0,0 +1,16 @@
+var bath = 0;
+var money = 0;
+function index() {
+  var price =
+  Document.getElementById('price').value;
+  var product =
+  Document.getElementById('Product Number')
+  bath = price*product;
+  money+=bath;
+  console.log(money);
+  Document.getElementById('bath').money;
+  console.log(bath);
+  price= Document.getElementById('price').value="";
+  product = Document.getElementById('product').value="";
+
+}
